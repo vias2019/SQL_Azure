06f2c3e9547df2fcf9ed54435c11daeb5899f04a
Victoria Grishkina <vias2010@gmail.com>
2022-05-13 16:05:12 -0700

    Updated

--- sql.js
+++ sql.js
@@ -1,56 +1,54 @@
-var Connection = require('tedious').Connection;  
-var config = {  
-    server: 'tcp:moviedb.database.windows.net',  //update me
-    authentication: {
-        type: 'default',
-        options: {
-            userName: 'project', //update me
-            password: 'tcss*545'  //update me
-        }
-    },
+const { Connection, Request } = require("tedious");
+
+// Create connection to database
+const config = {
+  authentication: {
     options: {
-        // If you are on Microsoft Azure, you need encryption:
-        encrypt: true,
-        database: 'movie'  //update me
-    }
-};  
-var connection = new Connection(config);  
-connection.on('connect', function(err) {  
-    // If no error, then good to proceed.
-    console.log("Connected");  
+      userName: "project", // update me
+      password: "tcss*545" // update me
+    },
+    type: "default"
+  },
+  server: "moviedb.database.windows.net", // update me
+  options: {
+    database: "movie", //update me
+    encrypt: true
+  }
+};
+
+const connection = new Connection(config);
+
+// Attempt to connect and execute queries if connection goes through
+connection.on("connect", err => {
+  if (err) {
+    console.error(err.message);
+  } else {
+    queryDatabase();
+  }
 });
 
-connection.connect();
+function queryDatabase() {
+  console.log("Reading rows from the Table...");
 
-var Request = require('tedious').Request;  
-var TYPES = require('tedious').TYPES;  
-  
-    function executeStatement() {  
-        request = new Request("SELECT * FROM GENRE;", function(err) {  
-        if (err) {  
-            console.log(err);}  
-        });  
-        var result = "";  
-        request.on('row', function(columns) {  
-            columns.forEach(function(column) {  
-              if (column.value === null) {  
-                console.log('NULL');  
-              } else {  
-                result+= column.value + " ";  
-              }  
-            });  
-            console.log(result);  
-            result ="";  
-        });  
-  
-        request.on('done', function(rowCount, more) {  
-        console.log(rowCount + ' rows returned');  
-        });  
-        
-        // Close the connection after the final event emitted by the request, after the callback passes
-        request.on("requestCompleted", function (rowCount, more) {
-            connection.close();
-        });
-        connection.execSql(request);  
-    }  
-    executeStatement() 
+  // Read all rows from table
+  const request = new Request(
+    `SELECT TOP (20) 
+     FROM [movie].[GENRE]`,
+    (err, rowCount) => {
+      if (err) {
+        console.error(err.message);
+      } else {
+        console.log(`${rowCount} row(s) returned`);
+      }
+    }
+  );
+
+  request.on("row", columns => {
+    columns.forEach(column => {
+      console.log("%s\t%s", column.metadata.colName, column.value);
+    });
+  });
+
+  connection.execSql(request);
+}
+queryDatabase()
